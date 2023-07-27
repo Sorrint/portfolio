ce0ac908a9bec1f73287f7361d31b46e9580c53f
Lebedev Anton <aslebedevdev@gmail.com>
2023-07-27 12:39:39 +0800

    change routes

--- src/App/routes.tsx
+++ src/App/routes.tsx
@@ -3,14 +3,10 @@ import { Navigate, RouteObject } from 'react-router-dom';
 import MainPage from 'pages/documents/components/mainPage';
 
 
-const BaseURL = ""
+const BaseURL = "/"
 
 const routes: RouteObject[] = [
-    { index: true, element: <Navigate to={`.${BaseURL}/main`} /> },
-    {
-        path: `${BaseURL}/main`,
-        element: <MainPage />,
-    },
-    { path: '*', element: <Navigate to={`./${BaseURL}/main`} /> }
+    { index: true, element: <MainPage /> },
+    { path: '*', element: <Navigate to={`./${BaseURL}`} /> }
 ];
 export default routes;
--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,5 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5000
-    },
-    base: '/portfolio'
+    }
 });
